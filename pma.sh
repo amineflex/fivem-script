79cc1885417f34594c3384f8a7f6644219ceb785
Amine B <59451119+ShotoYT@users.noreply.github.com>
2021-03-17 18:34:57 +0100

    Update pma.sh

--- pma.sh
+++ pma.sh
@@ -3,6 +3,24 @@
 #By VPSLabs
 
 echo "Installation phpMyAdmin | Par Amine B.";
+echo "";
+sleep 1
+
+echo "Installation Apache2";
+apt install sudo -y
+apt install apache2 apache2-doc -y
+apt install unzip -y
+wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
+echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo apt-get install ca-certificates apt-transport-https -y
+sleep 1
+
+echo "Installation php";
+sudo apt-get update
+sudo apt-get install php7.2 php7.2-cli php7.2-common php7.2-opcache php7.2-curl php7.2-mbstring php7.2-mysql php7.2-zip php7.2-xml -y
+
+sleep 3
+echo "Installation phpMyAdmin";
 cd /var/www/html/
 mkdir phpmyadmin
 cd phpmyadmin
