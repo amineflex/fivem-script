137c7c913ee533fb2a5e16f9b34cb582d169854e
Amine B <59451119+ShotoYT@users.noreply.github.com>
2021-03-30 03:37:34 +0200

    Update installFiveM.sh

--- installFiveM.sh
+++ installFiveM.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
+#
+# Script d'installation - Serveur FiveM
+# Par Adam - Kistin Soltuions
+#
+chmod 777 recipe1.sh
 
-echo "Mise à jour de Linux..."
 apt update -y
 apt install screen -y
 apt install -y tar xz-utils curl git file
-echo "Mise à jour terminée."
 
 mkdir -p /home/server/resources
 cd /home/server
 
-echo "updating citizenfx resource files"
-git clone https://github.com/citizenfx/cfx-server-data.git /tmp
-cp -Rf /tmp/resources/* resources/ 
+echo "Mise à jour des fichiers CITIZENFX"
+git clone https://github.com/citizenfx/cfx-server-data.git /cache
+cp -Rf /cache/resources/* resources/ 
 
 RELEASE_PAGE=$(curl -sSL https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/)
 
@@ -21,7 +24,7 @@ if [ "${FIVEM_VERSION}" == "latest" ] || [ -z ${FIVEM_VERSION} ] ; then
 else
     VERSION_LINK=$(echo -e "${RELEASE_PAGE}" | grep -Eo 'href=".*/*.tar.xz"' | grep -Eo '".*"' | sed 's/\"//g' | sed 's/\.\///1' | grep ${FIVEM_VERSION})
     if [ "${VERSION_LINK}" == "" ]; then
-        echo -e "defaulting to latest as the version requested was invalid."
+        echo -e "par défaut, car la version demandée n'était pas valable."
     else
         DOWNLOAD_LINK=$(echo https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/${VERSION_LINK})
     fi
@@ -29,10 +32,10 @@ fi
 
 if [ ! -z "${DOWNLOAD_URL}" ]; then 
     if curl --output /dev/null --silent --head --fail ${DOWNLOAD_URL}; then
-        echo -e "link is valid. setting download link to ${DOWNLOAD_URL}"
+        echo -e "Le lien est valide. Paramétrage du lien de téléchargement vers ${DOWNLOAD_URL}"
         DOWNLOAD_LINK=${DOWNLOAD_URL}
     else
-        echo -e "link is invalid closing out"
+        echo -e "le lien est invalide la fermeture"
         exit 2
     fi
 fi
@@ -41,45 +44,28 @@ echo -e "Running curl -sSL ${DOWNLOAD_LINK} -o ${DOWNLOAD_LINK##*/}"
 
 curl -sSL ${DOWNLOAD_LINK} -o ${DOWNLOAD_LINK##*/}
 
-echo "Extracting fivem files"
+echo "Extraction des fichiers"
 
-FILETYPE=$(file -F ',' ${DOWNLOAD_LINK##*/} | cut -d',' -f2 | cut -d' ' -f2)
-if [ "$FILETYPE" == "gzip" ]; then
-    tar xzvf ${DOWNLOAD_LINK##*/}
-elif [ "$FILETYPE" == "Zip" ]; then
-    unzip ${DOWNLOAD_LINK##*/}
-elif [ "$FILETYPE" == "XZ" ]; then
-    tar xvf ${DOWNLOAD_LINK##*/}
-else
-    echo -e "unknown filetype. Exiting"
-    exit 2 
-fi
-
-rm -rf ${DOWNLOAD_LINK##*/} run.sh
+tar xvf ${DOWNLOAD_LINK##*/}
+rm ${DOWNLOAD_LINK##*/}
 
 if [ -e server.cfg ]; then
-    echo "server config file exists"
+    echo "le fichier de configuration du serveur existe"
 else
-    echo "Downloading default fivem config"
+    echo "Téléchargement de la configuration par défaut de 5m"
     curl https://raw.githubusercontent.com/parkervcp/eggs/master/gta/fivem/server.cfg >> server.cfg
 fi
 
+
 mkdir logs/
 
-
-echo "Prenez le code FiveM et faites CTRL + A + D"
-sleep 3 
 screen -R fivem ./run.sh
 
-echo "Installation Apache2"
 apt update -y
 apt install sudo -y
 apt install apache2 apache2-doc -y
 apt install unzip -y
-echo "Installation Apache2 effectuée"
 
-
-echo "Installation PHP 7.2"
 wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
 echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
 sudo apt-get install ca-certificates apt-transport-https -y
@@ -91,21 +77,21 @@ sudo apt-get install php7.2 php7.2-cli php7.2-common php7.2-opcache php7.2-curl 
 
 sleep 3
 
-echo "Installation phpMyadmin"
 cd /var/www/html
 mkdir phpmyadmin
 cd /var/www/phpmyadmin/
+
 
 wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip
 unzip phpMyAdmin-5.0.2-all-languages.zip
 mv phpMyAdmin-5.0.2-all-languages/* /var/www/html/phpmyadmin
 rm -rf phpM*
-echo "Installation phpMyadmin terminée"
 
 sleep 3 
 
 apt-get install mysql-server -y
 
-rm -f /root/recipe1.sh
 
 echo "Installation complète"
+rm -f /root/recipe1.sh
+
